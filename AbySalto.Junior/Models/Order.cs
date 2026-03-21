81736d7a5bdc8758078146b169984aff86cdebde
ivasecko48 <robertivasecko2001@gmail.com>
2026-03-21 22:09:40 +0100

    dodani modeli narudzbe

---
+++ AbySalto.Junior/Models/Order.cs
@@ -0,0 +1,26 @@
+namespace AbySalto.Junior.Models
+{
+    public enum OrderStatus
+    {
+        Pending,        // Na čekanju
+        InPreparation,  // U pripremi
+        Completed       // Završena
+    }
+
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public DateTime OrderTime { get; set; } = DateTime.UtcNow;
+        public string DeliveryAddress { get; set; }
+        public string ContactNumber { get; set; }
+        public string? Note { get; set; }
+        public string Currency { get; set; } = "EUR";
+        public decimal TotalAmount => Items.Sum(i => i.Price * i.Quantity);
+
+        #region Reverse Navigation Properties
+        public List<OrderItem> Items { get; set; } = new();
+        #endregion
+    }
+}
---
+++ AbySalto.Junior/Models/OrderItem.cs
@@ -0,0 +1,15 @@
+namespace AbySalto.Junior.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public int OrderId { get; set; }
+
+        #region Navigation Properties
+        public Order Order { get; set; } = null!;
+        #endregion
+    }
+}
